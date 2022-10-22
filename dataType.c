94cc8788515d28fdaab5a2677645bdf180ce8680
sanjidaKabirSrity <sanjida.kabir.srity@gmail.com>
2022-10-22 18:58:58 +0600

    file add

---
+++ dataType.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int num1 = 45;
+    float num2 = 55.83F;
+    char ch = 's';
+
+    printf("%d Integer Number. %f Floating Number.\n", num1, num2);
+    printf("'%c' this is a character\n", ch);
+
+    return 0;
+}
--- variable.c
+++ variable.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
+#define PI 3.1416
 
 int main() {
     int num1 = 50;
     int num2 = 30;
-    
-    printf("\nNumber1 and Number2 Sum = %d\n", num1+num2);
+
+    printf("'%f' this is PI Number\n", PI);
+
+    const float g = 9.81;
+    printf("%f is Gravity Value \n", g);
+
+    printf("Number1 and Number2 Sum = %d\n", num1+num2);
     printf("Number1 and Number2 Subtraction = %d\n", num1-num2);
     printf("Number1 and Number2 Multiplication = %d\n", num1*num2);
-    printf("Number1 and Number2 Divide = %d\n\n", num1/num2);
+    printf("Number1 and Number2 Divide = %d\n", num1/num2);
 
     printf("Sum: %d+%d = %d\n", num1, num2, num1+num2);
     printf("Subtraction: %d-%d = %d\n", num1, num2, num1-num2);
     printf("Multiplication: %d*%d = %d\n", num1, num2, num1*num2);
-    printf("Divide: %d/%d = %d\n\n", num1, num2, num1/num2);
+    printf("Divide: %d/%d = %d\n", num1, num2, num1/num2);
 
     int num3;
     num3 = 25;
