558badd1d208b74e75b228b8c05b2ef1c661b6ad
sanjidaKabirSrity <sanjida.kabir.srity@gmail.com>
2022-11-05 22:51:50 +0600

    code add

--- condition.c
+++ condition.c
@@ -33,8 +33,72 @@ int main(){
     {
         printf("Other Color\n");
     }
+
+    int a, b, c;
+    printf("Enter 1st number for A: ");
+    scanf("%d", &a);
+    printf("Enter 2nd number for B: ");
+    scanf("%d", &b);
+    printf("Enter 3rd number for C: ");
+    scanf("%d", &c);
+    if(a > b&c){
+        printf("A is greater tahn B & C\n");
+    } else if(b > c&a){
+        printf("B is greater tahn C & A\n");
+    } else {
+        printf("C is greater tahn A & B\n");
+    }
+     
+
     
-    
+    // Switch statement
+    int day_num;
+    printf("Enter Day Number:");
+    scanf("%d", &day_num);
+
+    switch (day_num)
+    {
+    case 1:
+        printf("Sunday\n");
+        break;
+    case 2:
+        printf("Monday\n");
+        break;
+    case 3:
+        printf("Tuesday\n");
+        break;
+    case 4:
+        printf("Wednesday\n");
+        break;
+    case 5:
+        printf("Thursday\n");
+        break;
+    case 6:
+        printf("Friday\n");
+        break;
+    case 7:
+        printf("Saturday\n");
+        break;
+    default:
+        printf("Wrong input\n");
+        //break;
+    }
+
+    switch (day_num)
+    {
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+        printf("Work day\n");
+    case 6:
+    case 7:
+        printf("off day\n");
+        break;
+    default:
+        break;
+    }
 
     return 0;
 }
---
+++ even_odd_num.c
@@ -0,0 +1,15 @@
+#include "stdio.h"
+
+int main(){
+    int n;
+    printf("Enter An Integer Number: ");
+    scanf("%d", &n);
+
+    if(n%2 == 0) {
+        printf("%d is Even\n", n);
+    } else {
+        printf("%d is Odd\n", n);
+    }
+
+    return 0;
+}
